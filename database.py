5c195159919b511cbddaa47bf6f346448844b3e6
Dheeraj1632 <dheerajjangir63@gmail.com>
2024-07-28 20:58:44 +0000

    done

---
+++ database.py
@@ -0,0 +1,73 @@
+import pymysql
+import os
+
+my_secret = os.environ['PASSWORD']
+
+def load_jobs_from_db():
+    connection = pymysql.connect(
+        charset="utf8mb4",
+        connect_timeout=10,
+        cursorclass=pymysql.cursors.DictCursor,
+        db="defaultdb",
+        host="mysql-3337c7c9-dheerajjangir63-3fe9.g.aivencloud.com",
+        password=my_secret,
+        read_timeout=10,
+        port=25975,
+        user="avnadmin",
+        write_timeout=10,
+    )
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM jobs")
+        job = cursor.fetchall()
+        jobs = [dict(row) for row in job]
+
+    connection.close()
+
+    return jobs
+
+def load_job_from_db(id):
+    connection = pymysql.connect(
+        charset="utf8mb4",
+        connect_timeout=10,
+        cursorclass=pymysql.cursors.DictCursor,
+        db="defaultdb",
+        host="mysql-3337c7c9-dheerajjangir63-3fe9.g.aivencloud.com",
+        password=my_secret,
+        read_timeout=10,
+        port=25975,
+        user="avnadmin",
+        write_timeout=10,
+    )
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM jobs WHERE id = %s", (id,))
+        job = cursor.fetchone()
+
+    connection.close()
+
+    if job:
+        return dict(job)
+    else:
+        return None
+
+def add_application_to_db(job_id, data):
+    connection = pymysql.connect(
+        charset="utf8mb4",
+        connect_timeout=10,
+        cursorclass=pymysql.cursors.DictCursor,
+        db="defaultdb",
+        host="mysql-3337c7c9-dheerajjangir63-3fe9.g.aivencloud.com",
+        password=my_secret,
+        read_timeout=10,
+        port=25975,
+        user="avnadmin",
+        write_timeout=10,
+    )
+
+    with connection.cursor() as cursor:
+        query = "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(query, (job_id, data['full_name'], data['email'], data['linkedin_url'], data['education'], data['work_experience'], data['resume_url']))
+
+    connection.commit()
+    connection.close()
